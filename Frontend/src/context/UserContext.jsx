585fab80dfc8ea2a039bb0affa36f4736bc55a38
wd-rohan <sharmar42750@gmail.com>
2026-02-12 12:53:49 +0530

    Update UserContext.jsx

--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
 export const userDataContext = createContext();
 function UserContext({ children }) {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://ai-assistant-backend-7isc.onrender.com";
   const [userData, setUserData] = useState(null);
   const [frontendImage, setFrontendImage] = useState(null);
   const [backendImage, setBackendImage] = useState(null);
