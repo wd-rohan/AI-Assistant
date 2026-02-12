f7988c5f35c7408aaeb5528aa5f2cebc8a086219
wd-rohan <sharmar42750@gmail.com>
2026-02-12 12:59:07 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const port = Number(process.env.PORT) || 5000;
 app.use(express.json());
 
 app.use(cors(
-  {origin:"http://localhost:5173",
+  {origin:"https://ai-assistant-hnho.onrender.com",
     credentials:true
   }
 ))
